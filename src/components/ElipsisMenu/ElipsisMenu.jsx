45d653664b6f86566c7dc92261ccdd0c317e2fa4
GemsGame <a.masanovets@gmail.com>
2020-04-26 17:38:53 +0300

    feat: add styles

--- src/components/ElipsisMenu/ElipsisMenu.jsx
+++ src/components/ElipsisMenu/ElipsisMenu.jsx
@@ -1,35 +1,67 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faEllipsisH,
+    faEllipsisH, faProjectDiagram, faCog
 } from '@fortawesome/free-solid-svg-icons';
 import './_elipsisMenu.scss';
+import PropTypes from 'prop-types';
 
-const ElipsisMenu = (props) => {
-  const [visible, setVisible] = useState(() => false);
-  const openClose = () => {
-    setVisible(!visible);
-  };
-  let style = 'hidden';
-  if (visible) {
-    style = 'animated rubberBand';
-  }
-  return <div className="row mr-2 ml-2 mt-3 justify-content-end">
-        <div className="col-md-4 col-sm-8 col-lg-2">
-            <div className="row">
-                <div className="col text-right pointer"><FontAwesomeIcon icon={faEllipsisH} size="lg" color="#dddfeb" onClick={openClose}/></div>
-            </div>
-            <div className={`card dropdown-menu mr-2 ${style}`}>
-                <div className="card-body">
-                    <p className="card-title">Card title</p>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+class ElipsisMenu extends React.Component {
+    state = {
+        visible: false
+    }
+    closeW = () => {
+        this.setState(() => {
+            return { visible: false }
+        })
+    };
+    componentDidMount() {
+        document.addEventListener('click', (event) => {
+            console.log(event.target.id);
+            if (event.target.id !== 'dropdown-id') {
+            this.closeW();
+            }
+        });
+    }
+    openClose = () => {
+        this.setState(() => {
+            return { visible: !this.state.visible }
+        })
+    };
+  
+  
+    render() {
+        let style = 'hidden';
+        if (this.state.visible) {
+            style = 'animated rubberBand';
+        }
+        return <div className="row mr-2 ml-2 mt-3 justify-content-end">
+            <div className="col-md-4 col-sm-4 col-lg-2">
+                <div className="row">
+                    <div className="col text-right">
+                        <FontAwesomeIcon icon={faEllipsisH} size="lg" height="300" className="dropdown-button-pointer" id="dropdown-id" onClick={this.openClose} color="#dddfeb" />
+                        </div>
+                </div>
+                <div className={`card dropdown-menu mr-2 ${style}`}>
+                    <div className="card-body padding-05">
+                        <p className="card-title header text-center">Statistics</p>
+                         <ul className="list-group">
+                             <li className="list-group-item li-item"><div className="ml-3 mr-4">  <FontAwesomeIcon icon={faProjectDiagram} size="sm" height="300" className="dropdown-button-pointer mr-2" color="#dddfeb"/>All projects</div></li>
+                             <li className="list-group-item li-header"><div className="ml-3 mr-4">  <FontAwesomeIcon icon={faProjectDiagram} size="sm" height="300" className="dropdown-button-pointer mr-2" color="#dddfeb"/>Your projects</div>
+                             <ul className="list-group">
+                             <li className="list-group-item li-item"><div className="ml-5 mr-4">  <FontAwesomeIcon icon={faProjectDiagram} size="sm" height="300" className="dropdown-button-pointer mr-2" color="#dddfeb"/>All projects</div></li>
+                             <li className="list-group-item li-item"><div className="ml-5 mr-4">  <FontAwesomeIcon icon={faProjectDiagram} size="sm" height="300" className="dropdown-button-pointer mr-2" color="#dddfeb"/>All projects</div>
+                             
+                             </li>
+                         </ul>
+                             </li>
+                         </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>;
-};
-
+        </div>;
+    };
+}
 ElipsisMenu.propTypes = {
 
 };
